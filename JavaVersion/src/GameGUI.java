b01c74b3b7a0fa2884da141fff48ffa51569f897
naikajb <naika1492@gmail.com>
2023-08-12 00:59:13 -0400

    started new game implementation

--- JavaVersion/src/GameGUI.java
+++ JavaVersion/src/GameGUI.java
@@ -7,12 +7,12 @@ import java.io.Serializable;
 
 public class GameGUI implements Serializable {
 
-    JButton[][] buttons = new JButton[3][3];
-    JFrame game = null;
+    private static JButton[][] buttons = new JButton[3][3];
+    private static JFrame game = null;
     private static JFrame startUp = null;
     JPanel GamePanel = null;
 
-    public GameGUI(){
+    public GameGUI() {
         startUp = new JFrame("TIC-TAC-TOE");
         //game = new JFrame("TIC-TAC-TOE");
         setUpOpeningPanel();
@@ -33,69 +33,96 @@ public class GameGUI implements Serializable {
     /**
      * sets up the starting menu
      */
-    private void setUpOpeningPanel(){
-       startUp = new JFrame();
+    private void setUpOpeningPanel() {
+        startUp = new JFrame();
 
-       JButton start = new JButton("NEW GAME");
-       JButton resume = new JButton("RESUME");
-       JButton exit = new JButton("EXIT");
+        JButton start = new JButton("NEW GAME");
+        JButton resume = new JButton("RESUME");
+        JButton exit = new JButton("EXIT");
 
-       start.setBounds(200,160,100,40);
-       //System.out.println(start.getComponentCount());
-       start.setBackground(Color.BLUE);
-       start.setOpaque(true);
-       resume.setBounds(200,210,100,40);
-       exit.setBounds(200,260,100,40);
+        start.setBounds(200, 160, 100, 40);
+        resume.setBounds(200, 210, 100, 40);
+        exit.setBounds(400, 430, 90, 30);
 
-       start.addActionListener(new ActionListener() {
-           @Override
-           public void actionPerformed(ActionEvent e) {
-               startGame();
-           }
-       });
-       startUp.add(start);
+        start.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                startGame();
+            }
+        });
+        startUp.add(start);
 
-       startUp.add(resume);
+        //TODO implement resuming game
+        resume.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
 
-       exit.addActionListener(new ActionListener() {
-           @Override
-           public void actionPerformed(ActionEvent e) {
-               exitGame();
-           }
-       });
-       startUp.add(exit);
+            }
+        });
+        startUp.add(resume);
 
+        exit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                exitGame();
+            }
+        });
+        startUp.add(exit);
 
-       startUp.setSize(500,500);
-       startUp.setLayout(null);
-       startUp.setMaximumSize(new Dimension(500,500));
-       startUp.setMinimumSize(new Dimension(500,500));
-       startUp.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-       startUp.setVisible(true);
+        startUp.setSize(500, 500);
+        startUp.setLayout(null);
+        startUp.setMaximumSize(new Dimension(500, 500));
+        startUp.setMinimumSize(new Dimension(500, 500));
+        startUp.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        startUp.setVisible(true);
     }
 
     //TODO: adjust the color scheme of the 
-    private void initializeButtons(JPanel panel){
-        for (int row = 0; row < buttons.length; row++){
-            for(int col = 0; col < buttons.length; col++){
+    private static JPanel initializeButtons(JPanel panel) {
+        for (int row = 0; row < buttons.length; row++) {
+            for (int col = 0; col < buttons.length; col++) {
                 buttons[row][col] = new JButton();
                 buttons[row][col].setBackground(Color.GRAY);
                 buttons[row][col].setOpaque(true);
                 panel.add(buttons[row][col]);
             }
         }
+
+        return panel;
     }
 
     /**
      * switches window from startup menu to new Tic Tac Toe game
      */
-    private static void startGame(){
-       startUp.setVisible(false);
+    private static void startGame() {
+        startUp.setVisible(false);
+        game = new JFrame("TIC-TAC-TOE");
+        //Game idk = new Game();
+
+        setUpGameStart();
+        game.setSize(500, 500);
+        game.setLayout(null);
+
+        game.setMaximumSize(new Dimension(500, 500));
+        game.setMinimumSize(new Dimension(500, 500));
+        game.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        game.setVisible(true);
+
     }
 
-    private static void exitGame(){
+    private static void exitGame() {
         System.exit(0);
     }
+
+    private static void setUpGameStart(){
+        JPanel board = new JPanel();
+        board = initializeButtons(board);
+        board.setLayout(null);
+        board.setBounds(100,100,200,200);
+        game.add(board);
+
+    }
+
     public static void main(String[] args) {
         GameGUI g = new GameGUI();
     }
